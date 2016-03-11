7b8dafa3d6e14c2b13d52feaeb9ae07722d54c7f
mymyrenren <benson3188@gmail.com>
2016-03-11 19:36:23 +0800

    hw2

---
+++ 上課實作.R
@@ -0,0 +1,22 @@
+2^3
+3+1
+a <-4
+b<-2
+a+b
+x<-c(1,2,3)
+x+x
+y<-c(1,2,3,4)
+x+y
+z<-c(1,2,3,4,5,6)
+x+z
+sqrt(16)
+round(22.666667897,2)
+round(226684.8475,-4)
+class(a)
+is.character(a)
+x
+longx<-1:200
+length(longx)
+string<-"how are you?"
+length(string)
+nchar(string)
